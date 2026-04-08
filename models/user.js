9eccbccb93cf085f6c4e57fca589f200d3442968
wilzdude04-star <wilzdude04@gmail.com>
2026-04-08 14:00:40 +0100

    Add User model

---
+++ models/user.js
@@ -0,0 +1,11 @@
+// models/user.js
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, default: 'customer' },
+});
+
+module.exports = mongoose.model('User', UserSchema);
